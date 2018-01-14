bada0e5fa8a2b0a164600be12550495c1620937e
Lucas Britz <lucas.britz@hotmail.com>
2018-01-14 19:40:13 +0100

    Solved project.

--- assets/main.js
+++ assets/main.js
@@ -3,13 +3,40 @@ $(function() {
     url: 'https://www.codeschool.com/users/lucasbritz.json',
     dataType: 'jsonp',
     success: function(response) {
-      $.each(response.courses.completed, function(index, course) {
-        $('#badges').html('<div class="course">' + course + '</div>');
-        $('.course').html('<h3>' + course.title + '</h3>');
-        $('.course').html('<img src="' + course.badge + '" />');
-        $('.course').html('<a href="' + course.url
-          + '" target="_blank" class="btn btn-primary">See Course</a>');
-      });
+      addCourses(response.courses.completed);
     }
+  });
+
+  function addCourses(courses) {
+    let badges = $('#badges');
+
+    courses.forEach(function(course) {
+      let $course = $('<div />', {
+        'class': 'course'
+      }).appendTo(badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+    });
+  }
+
+  $.each(response.courses.completed, function(index, course) {
+    $('#badges').html('<div class="course">');
+    $('#badges').find('.course').html('<h3>' + course.title + '</h3>');
+    $('#badges').find('.course').html('<img src="' + course.badge + '" />');
+    $('#badges').find('.course').html('<a href="' + course.url
+      + '" target="_blank" class="btn btn-primary">See Course</a></div>');
   });
 });
