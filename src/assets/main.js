ee93d55239653738a37d61f751d2ee16881da86d
Lucas Britz <lucas.britz@hotmail.com>
2018-01-14 19:05:38 +0100

    Add course images.

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,6 +9,7 @@ $(function() {
       $.each(response.courses.completed, function(index, course) {
         $('#badges').html('<div class="course">' + course + '</div>');
         $('.course').html('<h3>' + course.title + '</h3>');
+        $('.course').html('<img src="' + course.badge + '" />');
       });
     }
   });
