61f34d95a0377349a182d4973374841bbebeef07
Lucas Britz <lucas.britz@hotmail.com>
2018-01-14 19:05:09 +0100

    Add course titles.

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,7 @@ $(function() {
     success: function(response) {
       $.each(response.courses.completed, function(index, course) {
         $('#badges').html('<div class="course">' + course + '</div>');
+        $('.course').html('<h3>' + course.title + '</h3>');
       });
     }
   });
