4bbe6d3c4ffd90914b830d1fcf92fcb2360f37e2
Lucas Britz <lucas.britz@hotmail.com>
2018-01-14 19:03:53 +0100

    Make an ajax call.

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,10 @@ $(function() {
 
   // your code will go here
 
+  $.ajax({
+    url: 'https://www.codeschool.com/users/lucasbritz.json',
+    dataType: 'jsonp',
+    success: function(response) {
+    }
+  });
 });
